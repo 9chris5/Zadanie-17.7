ecbafa19e7c14eb563934d4bb062470995fd16ea
Krzysiu <kheciek@gmail.com>
2017-12-15 11:37:42 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '14ulo97788ilpaassp80q7g3qe4d8dak.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'KxoPp4o1ILr2aVZmWwYKAFcb',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
